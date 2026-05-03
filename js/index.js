0885253cc842487f44a383040273037f889c50f5
PedrooGit <pedroogit@gmail.com>
2026-05-03 11:55:19 -0300

    V.1.0 - foi add coisa secretas

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var i = 0;
+
+function alerta(){
+    if(i >= 4){
+        alert("PARABENS VOCE DESCOBRIU NADA, VOCÊ É MUITO FOCADO EM DESCOBRIR NADA.");
+        i = 0;
+    }else{
+        alert("NAO TEM NADA");
+        i++;
+    }
+}
